ad2ac40ec29645cad90d51cf4787ddd87daa7c47
Kajsa Unge <hej@kajsaunge.se>
2017-07-15 09:44:16 +0200

    Adds animated bird svg. Separates styles

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,23 +7,31 @@ document.addEventListener('DOMContentLoaded', function() {
     frame.className += " clickOne";
   });
 
+var parallax = document.querySelectorAll(".parallax"),
+    speed = 0.5;
 
-  (function(){
+window.onscroll = function(){
+  [].slice.call(parallax).forEach(function(el,i){
 
-  var parallax = document.querySelectorAll(".parallax"),
-      speed = 0.5;
+    var windowYOffset = window.pageYOffset,
+        elBackgrounPos = "50% " + (windowYOffset * speed) + "px";
 
-  window.onscroll = function(){
-    [].slice.call(parallax).forEach(function(el,i){
+    el.style.backgroundPosition = elBackgrounPos;
 
-      var windowYOffset = window.pageYOffset,
-          elBackgrounPos = "50% " + (windowYOffset * speed) + "px";
+  });
+};
 
-      el.style.backgroundPosition = elBackgrounPos;
-
+    // scroll to bottom
+    document.querySelector('.js-scroll-to-about').addEventListener('click', function(e) {
+      e.preventDefault();
+      window.scrollTo({ bottom: document.body.clientHeight - window.innerHeight, right: 0, behavior: 'smooth' });
     });
-  };
 
-})();
+
+  // scroll to top
+  document.querySelector('.js-scroll-to-top').addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector('header').scrollIntoView({ behavior: 'smooth' });
+  });
 
 });
